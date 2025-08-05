2cc5a57602cc9e4da7194e078595e81ad641b06f
idruz <idrusramadhan24@gmail.com>
2025-08-05 14:31:27 +0700

    inputs

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 import os
 from google import genai
 from dotenv import load_dotenv
+import sys
 
 
 def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
+    
+    if len(sys.argv) < 2:
+        print("1")
+        sys.exit(1)
+    
+    prompt = " ".join(sys.argv[1:])
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+        contents=prompt
     )
     print("Prompt tokens:", response.usage_metadata.prompt_token_count)
     print("Response tokens:", response.usage_metadata.candidates_token_count)
@@ -18,4 +25,4 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    main()  
